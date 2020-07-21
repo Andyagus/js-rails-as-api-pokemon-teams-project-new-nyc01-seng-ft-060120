5167782d5c08dca1738718e8657e1c6d62d5d707
Andy Agus <Redagus@gmail.com>
2020-07-20 20:33:25 -0400

    ability to add pkemon

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -3,52 +3,47 @@ document.addEventListener('DOMContentLoaded', (event) => {
 	const BASE_URL = "http://localhost:3000"
 	const TRAINERS_URL = `${BASE_URL}/trainers`
 	const POKEMONS_URL = `${BASE_URL}/pokemons`
+	const main = document.querySelector('main')
 
 	const fetchTrainer = () => {
 		fetch (TRAINERS_URL)
 		.then(resp => resp.json())
 		.then(resp => resp.forEach(trainer => renderTrainers(trainer)))
+		// .then(resp => resp.forEach(pokemon => renderPokemon(pokemon)))
 	};
-
-	// const fetchPokemon = () => {
-	// 	fetch (POKEMON_URL)
-	// 	.then(resp => resp.json())
-	// 	.then(resp => resp.forEach(pokemon => renderPokemons(pokemon)))
-	// }
 
 
 	const renderTrainers = (trainer) => {
-
-		const id = trainer.id
-		console.log(id)
-		pokemon = trainer.pokemons.map(pokemon => `<li> ${pokemon.nickname} <button class="release"> "Release" </button> </li>`) 
-		main = document.querySelector('main')
-		card = document.createElement('div')
-		card.className = 'card'
-
-		card.innerHTML = `<p> ${trainer.name} </p> `
-		main.append(card)
-		pokemonUl = document.createElement('ul')
-		renderPokemon(pokemonUl)
-		
-		pokemonUl.innerHTML = `${pokemon.join('')} `
-
 		const addPokemonButton = document.createElement('button')
 		addPokemonButton.innerHTML = "Add Pokemon"
-		card.append(addPokemonButton)
-		card.append(pokemonUl)
+
+
+		let trainerCard = document.createElement('div')
+		trainerCard.className = 'card'
+		console.log(trainerCard)
+		const id = trainer.id
+		trainerCard.innerHTML = `<p> ${trainer.name} </p> `
+		trainerCard.append(addPokemonButton)
+		main.append(trainerCard)
+
+		//ul 
+		let pokeUl = document.createElement('ul')
+		trainerCard.append(pokeUl)
+		pokemon = trainer.pokemons.map(pokemon => renderPokemon(pokeUl, pokemon))
 	
 		addPokemonButton.addEventListener('click', e => {
-			addPokemon(id)
+			addPokemon(pokeUl, id)
+			console.log("clicked")
 		})
 	}
 
-	const renderPokemon = (listElement, pokemon) => {
-
+	const renderPokemon = (ulCard, pokemon) => {
+		pokeCard = document.createElement('li')
+		pokeCard.innerHTML = `${pokemon.nickname} <button class="release"> "Release" </button>`
+		ulCard.append(pokeCard)
 	}
 
-
-	const addPokemon = function(id){
+	const addPokemon = function(pokeUl, id){
 		fetch(POKEMONS_URL, {
 			method: 'POST',
 			headers: {
@@ -61,7 +56,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
 		})
 
 		.then(resp => resp.json())
-		.then(resp => pokemonUl.innerHTML = (`<li></li>`))
+		.then(resp => renderPokemon(pokeUl, resp))
 		
 	}
 		
@@ -69,3 +64,19 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
 })
 
+
+	// 	const renderList = () => {
+	// 	pokemonUl = document.createElement('ul')
+	// 	pokemonUl.innerHTML = `${pokemon.join('')} `
+	// 	const addPokemonButton = document.createElement('button')
+	// 	addPokemonButton.innerHTML = "Add Pokemon"
+
+	// 			// renderPokemon(pokemonUl)
+
+	// }
+
+
+	// const renderPokemon = (pokemon) => {
+	// 	pokemon = pokemon.pokemons.map(pokemon => `<li> ${pokemon.nickname} <button class="release"> "Release" </button> </li>`) 
+
+	// }
